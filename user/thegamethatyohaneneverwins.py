9bc8bc45e5419cde3410248406ee5252addce81f
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-03-08 10:35:25 +1100

    LYSTER, Matthew: 2018-03-08 10:35:25

--- user/thegamethatyohaneneverwins.py
+++ user/thegamethatyohaneneverwins.py
@@ -1,29 +1,28 @@
 player = input("Rock, Paper, or Scissors? (choose wisely): ")
-cheater = ["Rock", "Paper", "Scissors"]
 
-from random import choice, sample
-cheater = choice(list(set([0, 1, 2])))
+import random
+cheater = random.randint(0,2)
 
 if (player) == "Rock":
-    if (cheater) == "Rock":
+    if (cheater) == 0:
         print("It would appear that we have made the same choice")
-    elif (cheater) == "Paper":
+    elif (cheater) == 1:
         print("AHAHAHAHAHAHAHAHAHAHAHAHAHA YOU SUCK! I JUST BEAT YOU SENSELESS WITH THE POWER OF PAPER!")
-    elif (cheater) == "Scissors":
+    elif (cheater) == 2:
         print("...I don't wanna talk about it....")
 elif (player) == "Paper":
-    if (cheater) == "Rock":
+    if (cheater) == 0:
         print("I guess you win...")
-    elif (cheater) == "Paper":
+    elif (cheater) == 1:
         print("Don't copy me like that. Tie")
-    elif (cheater) == "Scissors":
+    elif (cheater) == 2:
         print("I eat paper like that for breakfast. YOU SUCK!")
 elif (player) == "Scissors":
-    if (cheater) == "Rock":
+    if (cheater) == 0:
         print("Well. That was unexpected. JUST KIDDING OF COURSE YOU LOST! YOU SUCK!")
-    elif (cheater) == "Paper":
+    elif (cheater) == 1:
         print("Well. I guess there is hope for you after all")
-    elif (cheater) == "Scissors":
+    elif (cheater) == 2:
         print("That is what you people call a tie")
 else:
     print("Please try again and acutally input 'Rock', 'Paper' or 'Scissors', (Make sure you use a capital letter at the start")
